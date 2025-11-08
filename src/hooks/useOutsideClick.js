2130c024c04477761657a1e3a1bc7324ab4745ea
imanrao90 <imanrao90@gmail.com>
2025-11-08 17:21:43 +0500

    fix: correct useOutsideClick conditional expression syntax

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -5,7 +5,9 @@ export function useOutsideClick(handler, listenCapturing = true) {
 
   useEffect(function () {
     function handleClick(e) {
-      if (ref.current && !ref.current.contains(e.target)) { console.log("outside click"), handler() }
+      if (ref.current && !ref.current.contains(e.target)) {
+        handler()
+      }
     }
 
     document.addEventListener("click", handleClick, listenCapturing)
